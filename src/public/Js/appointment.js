7a79111d254dee318d896ea686f7d1014aeac9a9
Anmol Verma <anmolverma102002@gmail.com>
2022-07-24 20:10:48 +0530

    Added google map iframe:rocket:

---
+++ src/public/Js/appointment.js
@@ -0,0 +1,10 @@
+function initMap() {
+  var location = {
+    lat: -25.363,
+    lng: 131.044,
+  };
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: location,
+  });
+}
