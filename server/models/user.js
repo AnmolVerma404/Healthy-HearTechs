09c1300255d0746705c60bbfb19c1f48c0e6b882
Anmol Verma <anmolverma102002@gmail.com>
2022-12-28 14:59:50 +0530

    feat: :sparkles: added schemas and completed user schema

---
+++ server/models/user.js
@@ -0,0 +1,42 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			minlength: 1,
+			trim: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		phone_number: {
+			type: String,
+			required: false,
+		},
+		email: {
+			type: String,
+			required: true,
+			minlength: 1,
+			unique: true,
+			trim: true,
+		},
+		appointment_id: {
+			type: Schema.Types.ObjectId,
+			required: false,
+		},
+	},
+	{
+		timestamps: true,
+		toJSON: {
+			transform(_, returnedData) {
+				returnedData.id = returnedData._id;
+				delete returnedData._id;
+			},
+		},
+	}
+);
+
+export const User = model("user",userSchema);
