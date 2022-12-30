7673c77def6ff82d9daafe2eeb85ec254cb9d27d
Anmol Verma <anmolverma102002@gmail.com>
2022-12-30 19:21:17 +0530

    feat: :sparkles: added appointment schema

---
+++ server/models/appointment.js
@@ -0,0 +1,34 @@
+import { Schema, model } from 'mongoose';
+
+const appointmentSchema = Schema(
+	{
+		doctor_id: {
+			type: Schema.Types.ObjectId,
+			required: true,
+			minlength: 1,
+			trim: true,
+		},
+		date: {
+			type: Date,
+            default: Date.now(),
+			required: true,
+		},
+		location: {
+			type: String,
+			required: true,
+            minlength:1,
+            trim:true
+		}
+	},
+	{
+		timestamps: true,
+		toJSON: {
+			transform(_, returnedData) {
+				returnedData.id = returnedData._id;
+				delete returnedData._id;
+			},
+		},
+	}
+);
+
+export const Appointment = model("appointment",appointmentSchema);
