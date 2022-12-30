904dbf04f7b915dc21585b88310e3aadf8af862f
Anmol Verma <anmolverma102002@gmail.com>
2022-12-30 23:17:09 +0530

    feat: :sparkles: added ayurvedic schema fixes #5

---
+++ server/models/ayurvedic.js
@@ -0,0 +1,33 @@
+import { Schema, model } from 'mongoose';
+
+const ayurvedicSchema = Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			minlength: 1,
+			trim: true,
+		},
+		dosage: {
+			type: String,
+			required: false,
+		},
+		side_effect: [
+			{
+				type: Schema.Types.ObjectId,
+				required: false,
+			},
+		],
+	},
+	{
+		timestamps: true,
+		toJSON: {
+			transform(_, returnedData) {
+				returnedData.id = returnedData._id;
+				delete returnedData._id;
+			},
+		},
+	}
+);
+
+export const Ayurvedic = model('ayurvedic', ayurvedicSchema);
