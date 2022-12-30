b83c4a204723502c8ef9b5bd70946b96a73914f6
techabhi08 <garg.abhinay8602@gmail.com>
2022-12-30 22:41:28 +0530

    feat: 🎉 #8 added symptom schema

---
+++ server/models/symptom.js
@@ -0,0 +1,23 @@
+import { Schema, model } from 'mongoose';
+
+const symptomSchema = Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			minlength: 1,
+			trim: true,
+		}
+	},
+	{
+		timestamps: true,
+		toJSON: {
+			transform(_, returnedData) {
+				returnedData.id = returnedData._id;
+				delete returnedData._id;
+			},
+		},
+	}
+);
+
+export const Symptom = model("symptom",symptomSchema);
