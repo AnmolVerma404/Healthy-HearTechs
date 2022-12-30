80ce5eb4db1063c6ba4df95801087e168125de24
techabhi08 <garg.abhinay8602@gmail.com>
2022-12-30 22:17:04 +0530

    Added the diseases model

---
+++ server/models/diseases.js
@@ -0,0 +1,35 @@
+import { Schema, model } from 'mongoose';
+
+const diseaseSchema = Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			minlength: 1,
+			trim: true,
+		},
+		symptoms_id: {
+			type: Schema.Types.ObjectId,
+			required: false,
+		},
+		causes_id: {
+			type: Schema.Types.ObjectId,
+			required: false,
+		},
+		appointment_id: {
+			type: Schema.Types.ObjectId,
+			required: false,
+		},
+	},
+	{
+		timestamps: true,
+		toJSON: {
+			transform(_, returnedData) {
+				returnedData.id = returnedData._id;
+				delete returnedData._id;
+			},
+		},
+	}
+);
+
+export const Diseases = model("diseases",diseaseSchema);
