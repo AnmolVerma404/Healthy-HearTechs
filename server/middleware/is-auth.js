75896619758ae4de66bd7c208ee5da3cd1b76eda
techabhi08 <garg.abhinay8602@gmail.com>
2023-01-29 18:15:45 +0530

    feat: :sparkles: implementing #23

---
+++ server/middleware/is-auth.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export const isAuth = function (req, res, next) {
+    const authHeader = req.get('Authorization');
+    if(!authHeader){
+        const error = new Error('Not authenticated.');
+        error.statusCode = 401;
+        throw error;
+    }
+    const token = authHeader.split(' ')[1];
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(token, process.env.JWT_KEY);
+    } catch(err) {
+        err.statusCode = 500;
+        throw err;
+    }
+    if(!decodedToken) {
+        const error = new Error('Node authenticated.');
+        error.statusCode = 401;
+        throw error;
+    }
+    req.userId = decodedToken.userId;
+    next();
+}
