1d8e7cacac090cf7354daf0a743169fa953cf9c6
Anmol Verma <anmolverma102002@gmail.com>
2023-01-30 13:17:55 +0530

    feat: :cookie: added JWT session-cookie validation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,22 +1,27 @@
-import express from "express";
-import { body } from "express-validator";
-import {signup, login} from "../controllers/auth.js";
+import express from 'express';
+import { body } from 'express-validator';
+import { signup, login, user, signout } from '../controllers/auth.js';
+import { isAuth } from '../middleware/is-auth.js';
 
 const router = express.Router();
 
 router.post(
-  "/signup",
-  [
-    body("email")
-      .isEmail()
-      .withMessage("Please enter a valid email.")
-      .normalizeEmail(),
-    body("password").trim().isLength({ min: 5 }),
-    body("name").trim().not().isEmpty(),
-  ],
-  signup
+	'/signup',
+	[
+		body('email')
+			.isEmail()
+			.withMessage('Please enter a valid email.')
+			.normalizeEmail(),
+		body('password').trim().isLength({ min: 5 }),
+		body('name').trim().not().isEmpty(),
+	],
+	signup
 );
 
 router.post('/login', login);
+
+router.get('/user', isAuth, user);
+
+router.post('/signout', signout);
 
 export default router;
