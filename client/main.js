21df42421c38561c85078a41f924f8818c78fbed
Anmol Verma <anmolverma102002@gmail.com>
2023-02-22 15:49:07 +0530

    feat: :tada: connected main page with backend

--- client/main.js
+++ client/main.js
@@ -1,19 +1,31 @@
-import axios from "axios";
-import { backendUrl } from "./src/utils/url";
+import axios from 'axios';
+import cookie from 'cookie';
+import { backendUrl } from './src/utils/url';
 
-const isAuth = document.getElementById('jsAuth');
-console.log(document.cookie);
-
-const isAuthenticated = async () =>{
-    try {
-        // const response = await axios.post(backendUrl+'/api/auth/user');
-        // console.log(response);
-    } catch (error) {
-        console.log('No cookie found');
-        isAuth.innerText = "Sign Up/In";
-        isAuth.href = "./src/views/signup.html";
-    }
-}
+const isAuthenticated = async () => {
+	const isAuth = document.getElementById('jsAuth');
+	const userEl = document.getElementById('jsUserName');
+	try {
+		const cookieObj = cookie.parse(document.cookie);
+		const response = await axios.post(backendUrl + '/api/auth/user', {
+			jwt: cookieObj.jwt,
+		});
+		console.log(response);
+		const name = response.data.data.name;
+		if (response.data.success) {
+			isAuth.innerText = 'Sign Out';
+			isAuth.href = '';
+			userEl.innerText = name.length > 6 ? name.substring(0, 5) + '...' : name;
+		} else {
+			isAuth.innerText = 'Sign Up/In';
+			isAuth.href = './src/views/signup.html';
+		}
+	} catch (error) {
+		console.log('No cookie found');
+		isAuth.innerText = 'Sign Up/In';
+		isAuth.href = './src/views/signup.html';
+	}
+};
 
 isAuthenticated();
 
