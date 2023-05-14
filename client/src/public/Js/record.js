b819ff52e8c802461f13f633870d05e3b99f2ae9
Anmol Verma <anmolverma102002@gmail.com>
2023-05-15 01:11:55 +0530

    feat: :sparkles: connected record backend and frontend data

--- client/src/public/Js/record.js
+++ client/src/public/Js/record.js
@@ -1,1 +1,18 @@
-console.log("Record");
+import axios from 'axios';
+import cookie from 'cookie';
+import { backendUrl } from '../../utils/url';
+
+const getAllRecord = async () =>{
+    try {
+		const cookieObj = cookie.parse(document.cookie);
+        const recordResp = await axios.post(backendUrl + '/api/record/allrecords',{
+			jwt: cookieObj.jwt,
+        });
+        console.log(recordResp);
+    } catch (error) {
+		console.log('No cookie found');
+        console.log(error);
+    }
+}
+
+getAllRecord();
--- server/routes/records.js
+++ server/routes/records.js
@@ -1,21 +1,22 @@
-import express from "express";
-import { body } from "express-validator";
+import express from 'express';
+import { body } from 'express-validator';
 
-import { isAuth } from "../middleware/is-auth.js";
-import { saveRecords } from "../controllers/records.js";
+import { isAuth } from '../middleware/is-auth.js';
+import { saveRecords, getAllRecords } from '../controllers/records.js';
 
 const router = express.Router();
 
 router.post(
-  "/",
-  isAuth,
-  [
-    body("doctorName").trim().isLength({ min: 3 }),
-    body("phone").trim().isLength({ min: 10 }),
-    body("hospitalName").trim().isLength({ min: 3 }),
-    body("medicalCondition").trim().isLength({ min: 3 }),
-  ],
-  saveRecords
+	'/',
+	isAuth,
+	[
+		body('doctorName').trim().isLength({ min: 3 }),
+		body('phone').trim().isLength({ min: 10 }),
+		body('hospitalName').trim().isLength({ min: 3 }),
+		body('medicalCondition').trim().isLength({ min: 3 }),
+	],
+	saveRecords
 );
 
+router.post('/allrecords', isAuth, getAllRecords);
 export default router;
